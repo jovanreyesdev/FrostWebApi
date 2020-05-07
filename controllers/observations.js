7e837d45c9ce8f546276381a22ee7c9be010e6c7
Jovan <jovan@sge.net.au>
2020-05-07 21:22:40 +0800

    added observations

--- controllers/observations.js
+++ controllers/observations.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const axios = require('../utils/axios');
 
 const router = express.Router();
 
+const { API_BASE_URI, API_CLIENT_ID } = process.env;
+
 const observationController = () => {
-  router.get('/get', (req, res) => {
-    res.send({ message: 'Hello World!' });
+  router.get('/get/:sourceId', async (req, res) => {
+
+    const { sourceId } = req.params;
+
+    const response = await axios
+      .get('/observations/v0.jsonld?sources=SN18700&referencetime=2019-11-25%2F2019-11-25&elements=air_temperature%2Cwind_speed%2Cboolean_fair_weather(cloud_area_fraction%20P1D)');
+
+    res.send({
+      ...response.data,
+    });
   });
 
   return router;
